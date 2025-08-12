3a5d4227d982c662394ff43903ff3c7bcc144f70
fernanda stocler <nandasastocler@gmail.com>
2025-08-12 16:45:27 -0300

    realizando etapa 19

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using app_tarefas.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace App_Tarefas.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Tipo> Tipos { get; set; } = default!;
+     public DbSet<Tarefa> Tarefas { get; set; } = default!;
+    
+}
